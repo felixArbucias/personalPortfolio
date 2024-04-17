a71d802213b8c77dd1b3049fb00b33cbee996d59
FelixArbucias <91673165+nameYAY0@users.noreply.github.com>
2024-04-17 08:39:34 -0400

    fixed file path errors

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { useState, useEffect} from "react";
-import { Container, Row } from "react-bootstrap";
-import { ArrowRightCircle} from "react-boostrap-icons";
+import react from 'react';
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { Col, Span } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
